676d0505976419a5014c0d7100443be55bde2f12
Mauricio Velazquez <20243ds142@utez.edu.mx>
2026-04-22 23:04:06 -0600

    aa
    Removed getter and setter methods for jugadorId and apuesta.

--- examen/examenMau/src/main/java/com/example/examenMau/dto/NuevaPartidaRequest.java
+++ examen/examenMau/src/main/java/com/example/examenMau/dto/NuevaPartidaRequest.java
@@ -5,9 +5,4 @@ public class NuevaPartidaRequest {
     private Long jugadorId;
     private double apuesta;
 
-    public Long getJugadorId() { return jugadorId; }
-    public void setJugadorId(Long jugadorId) { this.jugadorId = jugadorId; }
-
-    public double getApuesta() { return apuesta; }
-    public void setApuesta(double apuesta) { this.apuesta = apuesta; }
 }
